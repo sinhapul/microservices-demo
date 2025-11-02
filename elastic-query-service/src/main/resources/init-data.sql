093760ef7abd02784f90a7a97e92e557aa5fd541
Pulak Kr SInha <sinhapul@gmail.com>
2025-11-02 21:01:18 +0530

    web client in docker

--- elastic-query-service/src/main/resources/init-data.sql
+++ elastic-query-service/src/main/resources/init-data.sql
@@ -3,13 +3,13 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 INSERT INTO public.users(
 	id, username, firstname, lastname)
-	VALUES ('161ac4ee-0ebb-48c0-8c3d-ff2b9c9c60de', 'app_user', 'Standard', 'User');
+	VALUES ('82c41f3f-3206-412d-a7e5-c877a5860822', 'app_user', 'Standard', 'User');
 INSERT INTO public.users(
 	id, username, firstname, lastname)
-	VALUES ('306f3ff7-76fb-46c8-8b87-21adac612af5', 'app_admin', 'Admin', 'User');
+	VALUES ('610499e7-82f8-4569-bca7-87ee69cb6205', 'app_admin', 'Admin', 'User');
 INSERT INTO public.users(
 	id, username, firstname, lastname)
-	VALUES ('ca2c54b7-fc93-45ce-9812-1a0b2769fb66', 'app_super_user', 'Super', 'User');
+	VALUES ('261fb7a2-5b44-423d-b95b-14bf7634d1d1', 'app_super_user', 'Super', 'User');
 
 
 insert into documents(id, document_id)
@@ -20,18 +20,18 @@ insert into documents(id, document_id)
 values ('90573d2b-9a5d-409e-bbb6-b94189709a19', 3);
 
 insert into user_permissions(user_permission_id, user_id, document_id, permission_type)
-values (uuid_generate_v4(),'161ac4ee-0ebb-48c0-8c3d-ff2b9c9c60de', 'c1df7d01-4bd7-40b6-86da-7e2ffabf37f7', 'READ');
+values (uuid_generate_v4(),'82c41f3f-3206-412d-a7e5-c877a5860822', 'c1df7d01-4bd7-40b6-86da-7e2ffabf37f7', 'READ');
 
 insert into user_permissions(user_permission_id, user_id, document_id, permission_type)
-values (uuid_generate_v4(),'306f3ff7-76fb-46c8-8b87-21adac612af5', 'c1df7d01-4bd7-40b6-86da-7e2ffabf37f7', 'READ');
+values (uuid_generate_v4(),'610499e7-82f8-4569-bca7-87ee69cb6205', 'c1df7d01-4bd7-40b6-86da-7e2ffabf37f7', 'READ');
 
 insert into user_permissions(user_permission_id, user_id, document_id, permission_type)
-values (uuid_generate_v4(),'306f3ff7-76fb-46c8-8b87-21adac612af5', 'f2b2d644-3a08-4acb-ae07-20569f6f2a01', 'READ');
+values (uuid_generate_v4(),'610499e7-82f8-4569-bca7-87ee69cb6205', 'f2b2d644-3a08-4acb-ae07-20569f6f2a01', 'READ');
 
 insert into user_permissions(user_permission_id, user_id, document_id, permission_type)
-values (uuid_generate_v4(), '306f3ff7-76fb-46c8-8b87-21adac612af5', '90573d2b-9a5d-409e-bbb6-b94189709a19', 'READ');
+values (uuid_generate_v4(), '610499e7-82f8-4569-bca7-87ee69cb6205', '90573d2b-9a5d-409e-bbb6-b94189709a19', 'READ');
 
 insert into user_permissions(user_permission_id, user_id, document_id, permission_type)
-values (uuid_generate_v4(), 'ca2c54b7-fc93-45ce-9812-1a0b2769fb66', 'c1df7d01-4bd7-40b6-86da-7e2ffabf37f7', 'READ');
+values (uuid_generate_v4(), '261fb7a2-5b44-423d-b95b-14bf7634d1d1', 'c1df7d01-4bd7-40b6-86da-7e2ffabf37f7', 'READ');
 
 
